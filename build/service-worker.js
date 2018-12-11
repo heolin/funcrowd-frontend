c9b5d3cecb777da12bb6f3d76a7f58a901ca022a
Wojciech <wlodarczyk.woj@gmail.com>
2018-12-11 01:33:42 +0100

    Fixing build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c049aebe3ec30fdf3aca98a9cb55ac17.js"
+  "/precache-manifest.3ea3c28411862fc2fd0e0a23beb80f7a.js"
 );
 
 workbox.clientsClaim();
